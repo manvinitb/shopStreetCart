ec97caf5b8a857db3ae16bf2b01e0d82f3422a91
manvinitb <manvi.nitb@gmail.com>
2018-05-29 00:39:53 +0530

    logging configured

--- cart/src/main/java/com/shopstreet/backend/cart/CartApplication.java
+++ cart/src/main/java/com/shopstreet/backend/cart/CartApplication.java
@@ -1,12 +1,15 @@
 package com.shopstreet.backend.cart;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+        log.info("Logging is working!!!");
+    }
 }
